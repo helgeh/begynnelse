e6fb43d7c962477bfd01ec79268fce1d2d1d2567
Helge Hofstad <helge.hofstad@gmail.com>
2023-10-25 22:42:03 +0200

    forgot to remove outcommented

--- src/client/components/VuetifyAudioPlayer.vue
+++ src/client/components/VuetifyAudioPlayer.vue
@@ -150,19 +150,7 @@
 </template>
 
 <script>
-    import { throttle } from 'lodash'
-// function throttle (callback, limit) {
-//   var waiting = false;                      // Initially, we're not waiting
-//   return function () {                      // We return a throttled function
-//       if (!waiting) {                       // If we're not waiting
-//           callback.apply(this, arguments);  // Execute users function
-//           waiting = true;                   // Prevent future invocations
-//           setTimeout(function () {          // After a period of time
-//               waiting = false;              // And allow future invocations
-//           }, limit);
-//       }
-//   }
-// }
+import { throttle } from 'lodash'
 
 export default {
   props: {
