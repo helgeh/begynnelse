71c0eb2d295fa6eac24414b7ac7e94beff3833ba
Helge Hofstad <helge.hofstad@gmail.com>
2025-07-19 12:44:10 +0200

    fix nodemailer tls config

--- src/server/mailer.js
+++ src/server/mailer.js
@@ -2,7 +2,8 @@ import nodemailer from 'nodemailer'
 
 export async function sendMail({from, to, subject, body}) {
   const transporter = nodemailer.createTransport({
-    secure: !process.env.DEBUG_APP,
+    secure: false,
+    requireTLS: true,
     host: process.env.PROTON_SMTP_SERVER || 'smtp.ethereal.email',
     port: process.env.PROTON_SMTP_PORT || 587,
     auth: {
@@ -20,7 +21,8 @@ export async function sendMail({from, to, subject, body}) {
 
 export async function sendAdminMail(to, subject, body) {
   const transporter = nodemailer.createTransport({
-    secure: !process.env.DEBUG_APP,
+    secure: false,
+    requireTLS: true,
     host: process.env.PROTON_SMTP_SERVER || 'smtp.ethereal.email',
     port: process.env.PROTON_SMTP_PORT || 587,
     auth: {
@@ -28,15 +30,15 @@ export async function sendAdminMail(to, subject, body) {
       pass: process.env.PROTON_SMTP_TOKEN || '[PASSWORD]'
     }
   })
-  console.log('About to send a mail...')
-  console.dir({
-    secure: !process.env.DEBUG_APP,
-    host: process.env.PROTON_SMTP_SERVER || 'smtp.ethereal.email',
-    port: process.env.PROTON_SMTP_PORT || 587,
-    auth: {
-      user: process.env.PROTON_SMTP_USERNAME || '[USERNAME]'
-    }
-  })
+  // console.log('About to send a mail...')
+  // console.dir({
+  //   secure: !process.env.DEBUG_APP,
+  //   host: process.env.PROTON_SMTP_SERVER || 'smtp.ethereal.email',
+  //   port: process.env.PROTON_SMTP_PORT || 587,
+  //   auth: {
+  //     user: process.env.PROTON_SMTP_USERNAME || '[USERNAME]'
+  //   }
+  // })
   return await transporter.sendMail({
     from: '"Else N. Forba" <forb@nnel.se>',
     to,
