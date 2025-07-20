e97d4a7b81df65a68bb1329c44ff9d4c96b89a40
Helge Hofstad <helge.hofstad@gmail.com>
2025-07-20 18:01:38 +0200

    add slowness to user queries

--- src/server/api/users.js
+++ src/server/api/users.js
@@ -6,6 +6,12 @@ import { log } from '../logger.js'
 
 export default function configure(router) {
 
+  async function slowResponse() {
+    return new Promise(res => {
+      setTimeout(res, 1250)
+    })
+  }
+
   function notifyEmailVerify(to, secret) {
     const url = `https://begy.nnel.se/?email=${to}&token=${secret}#/verify`
     if (process.env.DEVELOP) {
@@ -23,6 +29,7 @@ export default function configure(router) {
   router.post('/blimed', async (req, res) => {
     const { usr, pw } = req.body
     const user = getUserByEmail(usr)
+    await slowResponse()
     if (user) {
       log(`Bruker fantes allerede (${usr})`)
       return res.status(409).json({ error: 'User exists' })
@@ -49,10 +56,12 @@ export default function configure(router) {
     const user = getUserByEmail(usr)
     if (!user || !user.password) {
       log(`Fant ingen bruker (${usr})`)
+      await slowResponse()
       return res.status(401).json({ error: 'Invalid credentials' })
     }
     if (await !checkPassword(pw, user.password)) {
       log(`Feil passord for bruker (${usr})`)
+      await slowResponse()
       return res.status(401).json({ error: 'Invalid credentials' })
     }
     const secret = randomHex(16)
@@ -77,6 +86,7 @@ export default function configure(router) {
       return res.json({ message: 'Verified!', success: true })
     }
     log(`Feil hemmelighet for bruker (${email})`)
+    await slowResponse()
     return res.status(400).json({ error: 'Not valid token' })
   })
 
@@ -85,32 +95,38 @@ export default function configure(router) {
     const user = getUserByEmail(usr)
     if (!user || !user.password) {
       log(`Fant ingen bruker (${usr})`)
+      await slowResponse()
       return res.status(404).json({ error: 'Not found' })
     }
     if (await checkPassword(pw, user.password)) {
       if (!user.details.startsWith('et:CONFIRMED-')) {
         log(`Bruker mangler verifisering (${usr})`)
+        await slowResponse()
         return res.status(403).json({ error: 'Email not verified' })
       }
       const token = makeTheToken(user.id, user.email)
       return res.json({ token, email: user.email, name: user.name })
     }
     log(`Feil usr/pw (${usr})`)
+    await slowResponse()
     return res.status(401).json({ error: 'Invalid credentials' })
   })
 
-  router.post('/slettmeg', authTheToken, (req, res) => {
+  router.post('/slettmeg', authTheToken, async (req, res) => {
     // TODO: Mark users as 'deleted' in stead of actually removing
     const result = removeUser(req.user.id)
+    await slowResponse()
     log(`Bruker slettet (${result.id, result.email})`)
     res.json({ message: `Slettet ${result.email}`, success: true })
   })
 
-  router.get('/meg', authTheToken, (req, res) => {
+  router.get('/meg', authTheToken, async (req, res) => {
     const me = getUserById(req.user.id)
+    if (!me)
+      await slowResponse()
     res.json({
-      name: me.name,
-      email: me.email,
+      name: me?.name || '',
+      email: me?.email || '',
     })
   })
 
