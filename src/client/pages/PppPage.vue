9e4e64823cbbd55ba9571e462ad16f7b1cb4bee9
Helge Hofstad <helge.hofstad@gmail.com>
2026-07-10 15:11:16 +0200

    add random autoplay

--- src/client/pages/PppPage.vue
+++ src/client/pages/PppPage.vue
@@ -10,6 +10,7 @@
     <v-row>
       <v-btn @click="fsClick">FS</v-btn>
       <v-btn :active="autoplay" @click="apClick">AP</v-btn>
+      <v-btn :active="random" @click="rndClick">RND</v-btn>
       <v-select label="interval" :items="[3, 5, 7, 10, 15, 30]" :model-value="interval" @update:modelValue="interval = $event" density="compact"></v-select>
     </v-row>
   </v-container>
@@ -28,6 +29,9 @@
   const lightbox = ref(null)
   const autoplay = ref(false)
   const interval = ref(5)
+  const random = ref(false)
+  let randomIndex = 0
+  let sequence = []
   let timeout = null
 
   function fsClick() {
@@ -38,6 +42,10 @@
     autoplay.value = !autoplay.value
   }
 
+  function rndClick() {
+    random.value = !random.value
+  }
+
   function runAutoplay() {
     if (timeout != null) {
       clearTimeout(timeout)
@@ -46,14 +54,46 @@
     timeout = setTimeout(() => {
       const pswp = lightbox.value.pswp
       if (!pswp) return
-      pswp.next()
+      if (!random.value)
+        pswp.next()
+      else
+        pswp.goTo(nextRandom())
       if (autoplay.value)
         runAutoplay()
     }, interval.value * 1000)
   }
 
+  function nextRandom() {
+    if (randomIndex >= sequence.length)
+      randomIndex = 0
+    return sequence[randomIndex++]
+  }
+
   onMounted(async () => {
     await fetchVideos()
+    initLightbox()
+  })
+
+  onUnmounted(() => {
+    if (lightbox.value) {
+      lightbox.value.destroy()
+      lightbox.value = null
+    }
+  })
+
+  async function fetchVideos() {
+    let tot = 0
+    const response = await service.getPppList()
+    galleries.value = response.folders
+    for (let i = 0; i < galleries.value.length; i++) {
+      const files = await service.getPppContents(response.folders[i])
+      items.value[galleries.value[i]] = files
+      tot += files.length
+    }
+    sequence = randomSequence(tot)
+  }
+
+  function initLightbox() {
     if (!lightbox.value) {
       lightbox.value = new PhotoSwipeLightbox({
         gallery: '#gallery',
@@ -69,37 +109,34 @@
           runAutoplay()
       })
     }
-  })
-
-  onUnmounted(() => {
-    if (lightbox.value) {
-      lightbox.value.destroy()
-      lightbox.value = null
-    }
-  })
-
-  async function fetchVideos() {
-    const response = await service.getPppList()
-    galleries.value = response.folders
-    for (let i = 0; i < galleries.value.length; i++) {
-      const files = await service.getPppContents(response.folders[i])
-      items.value[galleries.value[i]] = files
-    }
   }
 
   function requestFullScreen(element) {
     // Supports most browsers and their versions.
-    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen
 
     if (requestMethod) { // Native full screen.
-        requestMethod.call(element);
+        requestMethod.call(element)
     }
     else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        var wscript = new ActiveXObject("WScript.Shell");
+        var wscript = new ActiveXObject("WScript.Shell")
         if (wscript !== null) {
-            wscript.SendKeys("{F11}");
+            wscript.SendKeys("{F11}")
         }
     }
+  }
+
+  function randomSequence(len) {
+    let currentIndex = len
+    let i = 1
+    const array = Array.from({length: len}, () => i++)
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]]
+    }
+    return array
   }
 </script>
 
